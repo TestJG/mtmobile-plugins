a043e6ee1c588ccb24677808f41aac5952579230
JBerg <josep.bergay@gmail.com>
2022-05-31 13:18:52 +0200

    feat(guid): add ios implementation

--- packages/nativescript-guid/index.ios.ts
+++ packages/nativescript-guid/index.ios.ts
@@ -1,5 +1,1 @@
-import { NativescriptGuidCommon } from './common';
-
-export class NativescriptGuid extends NativescriptGuidCommon {
-    
-}
+export const createGuid = (): string => NSUUID.new().UUIDString;
