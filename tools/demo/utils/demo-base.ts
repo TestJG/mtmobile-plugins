77c0852191ac71810c883517072a65449539a327
JBerg <josep.bergay@gmail.com>
2022-06-02 13:19:26 +0200

    chore(utils): add safeAction method to demobase

--- tools/demo/utils/demo-base.ts
+++ tools/demo/utils/demo-base.ts
@@ -2,4 +2,14 @@ import { Observable } from '@nativescript/core';
 
 export class DemoSharedBase extends Observable {
   // in case you want to globally control how your shared demo code works across whole workspace
+
+  async safeAction<T>(action: () => Promise<T>, log = false) {
+    try {
+      const result = await action();
+      log && console.log(result);
+      return result;
+    } catch (error) {
+      log && console.log(error);
+    }
+  }
 }
