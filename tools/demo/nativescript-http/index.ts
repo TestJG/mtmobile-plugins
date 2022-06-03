c7c9747bd4bc8603410e41a79f3442e4fc07a483
JBerg <josep.bergay@gmail.com>
2022-06-03 14:42:38 +0200

    test(http): use plugin from demo apps

--- tools/demo/nativescript-http/index.ts
+++ tools/demo/nativescript-http/index.ts
@@ -1,8 +1,24 @@
+import { createHttp } from '@testjg/nativescript-http';
+import { firstValueFrom } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { DemoSharedBase } from '../utils';
-import {} from '@testjg/nativescript-http';
 
 export class DemoSharedNativescriptHttp extends DemoSharedBase {
+  http = createHttp('https://jsonplaceholder.typicode.com');
+  maxTodos = 200;
+
   testIt() {
-    console.log('test nativescript-http!');
+    this.safeAction(async () => {
+      const id = Math.floor(Math.random() * this.maxTodos) + 1;
+      const json$ = this.http
+        .request({
+          method: 'GET',
+          url: `todos/${id}`,
+          headers: { 'Accept-Language': ['ca', 'en'] },
+        })
+        .pipe(switchMap((res) => res.json()));
+
+      return firstValueFrom(json$);
+    }, true);
   }
 }
