107aa9edc4e8273a75018b2ce4d427e71e96bc37
JBerg <josep.bergay@gmail.com>
2022-06-07 14:08:37 +0200

    feat(nfc): export raw provider for older versions of angular

--- packages/nativescript-nfc/angular/index.ts
+++ packages/nativescript-nfc/angular/index.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Provider } from '@angular/core';
 import { NfcService } from '@testjg/nativescript-nfc';
 
 @Injectable()
 export class NgNfcService extends NfcService {}
+
+export const NfcProvider: Provider = { provide: NfcService, useClass: NfcService };
